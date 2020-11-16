4374705ad4b1013787a7ab6684386b884794ab0f
Julien Simonet <j.mons54@gmail.com>
2020-11-16 12:29:18 +0100

    add navabar action

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -6,13 +6,42 @@
       <span>mizogoo</span>
     </b-navbar-brand>
     <b-navbar-nav>
-
+      <b-nav-item
+        href="#">
+        <b-icon-arrow-left-short/>
+        Retour
+      </b-nav-item>
+      <b-nav-item>Télécharger</b-nav-item>
+    </b-navbar-nav>
+    <b-navbar-nav class="ml-auto">
+      <b-nav-item  v-if="onSave" :disabled="true">
+        Enregistrement...
+      </b-nav-item>
+      <b-nav-item  v-else-if="saved" :disabled="true">
+        Enregistré
+      </b-nav-item>
+      <b-nav-item v-else @click="saveMenuData">
+        Enregistrer
+      </b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
 
 <script>
+import { mapActions, mapState } from 'vuex'
+
 export default {
+  computed: {
+    ...mapState({
+      onSave: state => state.menu.onSave,
+      saved: state => state.menu.saved,
+    }),
+  },
+  methods: {
+    ...mapActions('menu', [
+      'saveMenuData',
+    ]),
+  },
 }
 </script>
 
