1366b65942f3734862569e9c994b698bf839e5db
Julien Simonet <j.mons54@gmail.com>
2021-01-01 23:38:07 +0100

    fix

--- src/components/Images.vue
+++ src/components/Images.vue
@@ -24,7 +24,9 @@
               ...value,
               size: {
                 left: 0,
+                right: 0,
                 top: 0,
+                bottom: 0,
                 width: value.image.webformatWidth,
                 height: value.image.webformatHeight,
               },
